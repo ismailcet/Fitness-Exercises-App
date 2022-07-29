7496187d9df292b56994264fe7dc291ce15cc1bd
ismailcet <ismailcet3@gmail.com>
2022-07-29 23:34:05 +0300

    Footer Section is over.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Typography
+        variant="h5"
+        sx={{ fontSize: { lg: "18px", xs: "10px" } }}
+        marginTop="64px"
+        textAlign="center"
+        pb="40px"
+      >
+        Made with by Ismailcet
+      </Typography>
+    </Box>
+  );
 };
 
 export default Footer;
